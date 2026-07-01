104c097bc31cd3977c0fd94e310d77f5b681083c
Shamim <shamimtabasi6@gmail.com>
2026-07-01 15:49:02 +0330

    Auto Save 2026-07-01 15:49:02

--- Modules/Users/routes/web.php
+++ Modules/Users/routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Users\Http\Controllers\UsersController;
 
+/**
+ * Web routes (Inertia panel)
+ */
 Route::middleware(['web', 'auth'])
-    ->prefix('users')
+    ->prefix('admin/users')
     ->name('users.')
-    ->group(function (): void {
-        //
+    ->group(function () {
+
+        Route::get('/', [UsersController::class, 'index'])->name('index');
+        Route::get('/create', [UsersController::class, 'create'])->name('create');
+        Route::post('/', [UsersController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [UsersController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [UsersController::class, 'update'])->name('update');
+        Route::delete('/{id}', [UsersController::class, 'destroy'])->name('destroy');
     });
