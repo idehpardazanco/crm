a2e7ebef90569af68dfe6664bb5e5849ad710067
Shamim <shamimtabasi6@gmail.com>
2026-07-01 16:21:38 +0330

    Auto Save 2026-07-01 16:21:38

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,51 +1,8 @@
 <?php
 
-namespace Modules\Users\Http\Controllers;
+namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Modules\Users\Services\UserService;
-
-/**
- * Users Controller (thin controller pattern)
- */
-class UsersController
+abstract class Controller
 {
-    public function __construct(
-        private UserService $service
-    ) {}
-
-    public function index()
-    {
-        return $this->service->list();
-    }
-
-    public function create()
-    {
-        return $this->service->createView();
-    }
-
-    public function store(Request $request)
-    {
-        return $this->service->create($request->all());
-    }
-
-    public function edit(int $id)
-    {
-        return $this->service->editView($id);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        return $this->service->update($id, $request->all());
-    }
-
-    public function destroy(int $id)
-    {
-        return $this->service->delete($id);
-    }
-
-    public function show(int $id)
-    {
-        return $this->service->find($id);
-    }
+    //
 }
