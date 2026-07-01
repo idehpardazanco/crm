d480eb872f6d6d9877859205c283144c6aead8b8
Shamim <shamimtabasi6@gmail.com>
2026-07-01 16:41:27 +0330

    Auto Save 2026-07-01 16:41:26

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('auths', AuthController::class)->names('auth');
-});
+Route::prefix('v1/auth')
+    ->group(function () {
+
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/otp/send', [AuthController::class, 'sendOtp']);
+        Route::post('/otp/verify', [AuthController::class, 'verifyOtp']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
