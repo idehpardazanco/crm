865d14ce4c368db1892e827de307a717a90f96f9
Shamim <shamimtabasi6@gmail.com>
2026-07-01 16:41:04 +0330

    Auto Save 2026-07-01 16:41:04

--- Modules/Auth/routes/web.php
+++ Modules/Auth/routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('auths', AuthController::class)->names('auth');
-});
+Route::prefix('auth')
+    ->middleware(['web'])
+    ->group(function () {
+
+        Route::get('/login', [AuthController::class, 'loginPage'])->name('auth.login');
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::post('/otp/send', [AuthController::class, 'sendOtp']);
+        Route::post('/otp/verify', [AuthController::class, 'verifyOtp']);
+
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
