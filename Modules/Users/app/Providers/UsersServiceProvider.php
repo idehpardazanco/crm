9a3c507174e4b00bbaf56233ac39ded89709916f
Shamim <shamimtabasi6@gmail.com>
2026-07-02 12:47:16 +0330

    Auto Save 2026-07-02 12:47:16

--- Modules/Users/app/Providers/UsersServiceProvider.php
+++ Modules/Users/app/Providers/UsersServiceProvider.php
@@ -2,45 +2,20 @@
 
 namespace Modules\Users\app\Providers;
 
-use Nwidart\Modules\Support\ModuleServiceProvider;
-use Illuminate\Console\Scheduling\Schedule;
+use App\Support\BaseModuleServiceProvider;
+use Modules\Users\Services\UserService;
+use Modules\Users\Repositories\UserRepository;
 
-class UsersServiceProvider extends ModuleServiceProvider
+class UsersServiceProvider extends BaseModuleServiceProvider
 {
-    /**
-     * The name of the module.
-     */
-    protected string $name = 'Users';
+    public function register(): void
+    {
+        $this->app->singleton(UserRepository::class);
+        $this->app->singleton(UserService::class);
+    }
 
-    /**
-     * The lowercase version of the module name.
-     */
-    protected string $nameLower = 'users';
-
-    /**
-     * Command classes to register.
-     *
-     * @var string[]
-     */
-    // protected array $commands = [];
-
-    /**
-     * Provider classes to register.
-     *
-     * @var string[]
-     */
-    protected array $providers = [
-        EventServiceProvider::class,
-        RouteServiceProvider::class,
-    ];
-
-    /**
-     * Define module schedules.
-     * 
-     * @param $schedule
-     */
-    // protected function configureSchedules(Schedule $schedule): void
-    // {
-    //     $schedule->command('inspire')->hourly();
-    // }
+    public function boot(): void
+    {
+        $this->loadModule(__DIR__ . '/..');
+    }
 }
