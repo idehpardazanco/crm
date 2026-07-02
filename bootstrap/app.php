f8ad9cac9b32e0e24e3538aff6acd95a9350143b
Shamim <shamimtabasi6@gmail.com>
2026-07-02 16:21:00 +0330

    Auto Save 2026-07-02 16:21:00

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        
+        $middleware->append(
+            \Modules\Monitoring\Middleware\RequestLoggerMiddleware::class
+        );
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
