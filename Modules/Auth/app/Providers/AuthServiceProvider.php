55ec3b2748b0334b2cbdf67f6f4ec6eb88618a9b
Shamim <shamimtabasi6@gmail.com>
2026-07-02 12:48:01 +0330

    Auto Save 2026-07-02 12:48:01

--- Modules/Auth/app/Providers/AuthServiceProvider.php
+++ Modules/Auth/app/Providers/AuthServiceProvider.php
@@ -1,46 +1,21 @@
 <?php
 
-namespace Modules\Auth\app\Providers;
+namespace Modules\Auth\Providers;
 
-use Nwidart\Modules\Support\ModuleServiceProvider;
-use Illuminate\Console\Scheduling\Schedule;
+use App\Support\BaseModuleServiceProvider;
+use Modules\Auth\Services\AuthService;
+use Modules\Auth\Services\OtpService;
 
-class AuthServiceProvider extends ModuleServiceProvider
+class AuthServiceProvider extends BaseModuleServiceProvider
 {
-    /**
-     * The name of the module.
-     */
-    protected string $name = 'Auth';
+    public function register(): void
+    {
+        $this->app->singleton(AuthService::class);
+        $this->app->singleton(OtpService::class);
+    }
 
-    /**
-     * The lowercase version of the module name.
-     */
-    protected string $nameLower = 'auth';
-
-    /**
-     * Command classes to register.
-     *
-     * @var string[]
-     */
-    // protected array $commands = [];
-
-    /**
-     * Provider classes to register.
-     *
-     * @var string[]
-     */
-    protected array $providers = [
-        EventServiceProvider::class,
-        RouteServiceProvider::class,
-    ];
-
-    /**
-     * Define module schedules.
-     * 
-     * @param $schedule
-     */
-    // protected function configureSchedules(Schedule $schedule): void
-    // {
-    //     $schedule->command('inspire')->hourly();
-    // }
+    public function boot(): void
+    {
+        $this->loadModule(__DIR__ . '/..');
+    }
 }
