5fb3b7039c1286b371be144fbd5b01d251211a81
Shamim <shamimtabasi6@gmail.com>
2026-07-02 11:32:07 +0330

    Auto Save 2026-07-02 11:32:07

--- Modules/Settings/app/Providers/RouteServiceProvider.php
+++ Modules/Settings/app/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Settings\Providers;
+namespace Modules\Settings\app\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
