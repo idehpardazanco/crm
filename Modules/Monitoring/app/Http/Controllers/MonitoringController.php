a9dfe1fbd0bc1f67686c5a5ac897b4587a1c1596
Shamim <shamimtabasi6@gmail.com>
2026-07-05 11:16:20 +0330

    Auto Save 2026-07-05 11:16:20

--- Modules/Monitoring/app/Http/Controllers/MonitoringController.php
+++ Modules/Monitoring/app/Http/Controllers/MonitoringController.php
@@ -55,20 +55,19 @@ class MonitoringController
      */
     public function requestLogs(Request $request)
     {
-        return RequestLog::query()
-            ->when($request->status, function ($q) {
-                $q->where('status_code', request('status'));
-            })
-            ->when($request->search, function ($q) {
-                $q->where('url', 'like', '%'.request('search').'%');
-            })
-            ->when($request->from, function ($q) {
-                $q->whereDate('created_at', '>=', request('from'));
-            })
-            ->when($request->to, function ($q) {
-                $q->whereDate('created_at', '<=', request('to'));
-            })
+        $data = RequestLog::query()
+            ->when($request->status, fn($q) => $q->where('status_code', $request->status))
+            ->when($request->search, fn($q) => $q->where('url', 'like', "%{$request->search}%"))
             ->latest()
             ->paginate($request->per_page ?? 15);
+
+        return [
+            'data' => $data->items(),
+            'meta' => [
+                'current_page' => $data->currentPage(),
+                'last_page' => $data->lastPage(),
+                'total' => $data->total(),
+            ]
+        ];
     }
 }
