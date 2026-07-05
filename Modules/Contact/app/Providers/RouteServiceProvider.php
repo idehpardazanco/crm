afeeb5c5f44eb6e1a9f0001778ffae8a19f3cfc7
Shamim <shamimtabasi6@gmail.com>
2026-07-05 15:57:07 +0330

    Auto Save 2026-07-05 15:57:07

--- Modules/Contact/app/Providers/RouteServiceProvider.php
+++ Modules/Contact/app/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Contact\Providers;
+namespace Modules\Contact\app\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
