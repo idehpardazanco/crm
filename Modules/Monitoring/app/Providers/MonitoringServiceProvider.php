cda05a4a6217cbe25c7a1cc9ded9760c084a07f8
Shamim <shamimtabasi6@gmail.com>
2026-07-05 15:41:42 +0330

    Auto Save 2026-07-05 15:41:42

--- Modules/Monitoring/app/Providers/MonitoringServiceProvider.php
+++ Modules/Monitoring/app/Providers/MonitoringServiceProvider.php
@@ -3,7 +3,11 @@
 namespace Modules\Monitoring\app\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Support\BaseModuleServiceProvider;
+use Modules\Auth\Services\AuthService;
+use Modules\Auth\Services\OtpService;
 
+class AuthServiceProvider extends BaseModuleServiceProvider
 class MonitoringServiceProvider extends ServiceProvider
 {
     public function register(): void
