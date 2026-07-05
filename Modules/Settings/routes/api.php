bfd05053a11f907d01dd3f5a82cd92d5186892c4
Shamim <shamimtabasi6@gmail.com>
2026-07-05 14:25:26 +0330

    Auto Save 2026-07-05 14:25:26

--- Modules/Settings/routes/api.php
+++ Modules/Settings/routes/api.php
@@ -7,3 +7,5 @@ use Modules\Settings\app\Http\Controllers\SettingsController;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('settings', SettingsController::class)->names('settings');
 });
+Route::get('/settings/sms', [SettingsController::class, 'sms']);
+Route::post('/settings/sms', [SettingsController::class, 'updateSms']);
