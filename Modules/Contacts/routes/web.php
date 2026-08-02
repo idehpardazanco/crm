f29d02cd9a086943f1550f8a98c1c579d23c1b3c
Shamim <shamimtabasi6@gmail.com>
2026-08-02 15:18:50 +0330

    Auto Save 2026-08-02 15:18:50

--- Modules/Contacts/routes/web.php
+++ Modules/Contacts/routes/web.php
@@ -3,6 +3,35 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Contacts\app\Http\Controllers\ContactsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('contacts', ContactsController::class)->names('contacts');
-});
+
+Route::middleware('auth')
+    ->prefix('contacts')
+    ->name('contacts.')
+    ->group(function(){
+
+
+        Route::get('/',[
+            ContactsController::class,
+            'index'
+        ])->name('index');
+
+
+        Route::post('/',[
+            ContactsController::class,
+            'store'
+        ])->name('store');
+
+
+        Route::put('/{id}',[
+            ContactsController::class,
+            'update'
+        ])->name('update');
+
+
+        Route::delete('/{id}',[
+            ContactsController::class,
+            'destroy'
+        ])->name('destroy');
+
+
+    });
