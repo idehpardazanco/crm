b81adda8f6dc4efc95a84f012fe9e65563888136
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:02:18 +0330

    Auto Save 2026-08-02 14:02:18

--- Modules/Monitoring/routes/web.php
+++ Modules/Monitoring/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Monitoring\Http\Controllers\MonitoringController;
+use Modules\Monitoring\app\Http\Controllers\MonitoringController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('monitorings', MonitoringController::class)->names('monitoring');
