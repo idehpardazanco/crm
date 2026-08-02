65e4dba999a0b8fe582136b4224062b5b71ee210
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:14:37 +0330

    Auto Save 2026-08-02 14:14:37

--- Modules/Users/routes/api.php
+++ Modules/Users/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Users\Http\Controllers\UsersController;
+use Modules\Users\app\Http\Controllers\UsersController;
 
 /**
  * API routes (future mobile / external panel)
