e77f7e9d397668952cc8fc447625b8f3286604b7
Shamim <shamimtabasi6@gmail.com>
2026-08-02 15:11:14 +0330

    Auto Save 2026-08-02 15:11:14

--- Modules/Contacts/database/migrations/2026_07_05_125508_create_contacts_table.php
+++ Modules/Contacts/database/migrations/2026_07_05_125508_create_contacts_table.php
@@ -6,27 +6,70 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('contacts', function ($table) {
+        Schema::create('contacts', function (Blueprint $table) {
+
             $table->id();
 
-            $table->string('name');
-            $table->string('mobile')->index();
-            $table->string('email')->nullable();
+            $table->string('business_name')
+                ->nullable();
 
-            $table->text('address')->nullable();
+            $table->string('name');
+
+            $table->string('mobile')
+                ->index();
+
+            $table->string('phone')
+                ->nullable();
+
+            $table->string('email')
+                ->nullable();
+
+
+            $table->string('city')
+                ->nullable();
+
+
+            $table->string('category')
+                ->nullable();
+
+
+            $table->string('source')
+                ->nullable();
+
+
+            $table->enum('status', [
+                'new',
+                'active',
+                'inactive',
+                'customer'
+            ])
+            ->default('new');
+
+
+            $table->foreignId('assigned_user_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+
+            $table->text('address')
+                ->nullable();
+
+
+            $table->text('description')
+                ->nullable();
+
 
             $table->timestamps();
+
+            $table->softDeletes();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('contacts');
