b8be6a00dfb9e317aff94841cabb9ea66495be83
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:11:28 +0330

    Auto Save 2026-08-02 14:11:28

--- Modules/Orders/routes/web.php
+++ Modules/Orders/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Orders\Http\Controllers\OrdersController;
+use Modules\Orders\app\Http\Controllers\OrdersController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('orders', OrdersController::class)->names('orders');
