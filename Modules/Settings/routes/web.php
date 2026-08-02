05e7c4bdd26a9ef7c1d63ecf1063cdfa28fee985
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:10:52 +0330

    Auto Save 2026-08-02 14:10:52

--- Modules/Settings/routes/web.php
+++ Modules/Settings/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Settings\Http\Controllers\SettingsController;
+use Modules\Settings\app\Http\Controllers\SettingsController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('settings', SettingsController::class)->names('settings');
