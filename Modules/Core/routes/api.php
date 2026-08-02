7c1d67b3fcbe7268384c007369571f517be7697b
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:11:14 +0330

    Auto Save 2026-08-02 14:11:14

--- Modules/Core/routes/api.php
+++ Modules/Core/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Core\Http\Controllers\CoreController;
+use Modules\Core\app\Http\Controllers\CoreController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('cores', CoreController::class)->names('core');
