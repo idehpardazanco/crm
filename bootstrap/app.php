2a8cf4f44048ce26cceca5eaf6bb5950656020de
Shamim <shamimtabasi6@gmail.com>
2026-08-02 13:05:34 +0330

    Auto Save 2026-08-02 13:05:34

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
          */
         $middleware->append([
             \Modules\Monitoring\app\Middleware\RequestLoggerMiddleware::class,
-            \App\Http\Middleware\EnsureMonitoringAccess::class,
+            // \App\Http\Middleware\EnsureMonitoringAccess::class,
 
         ]);
 
