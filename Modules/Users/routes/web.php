7ed9bd5de84b7749e8741ac8db7d34ec633e0264
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:01:48 +0330

    Auto Save 2026-08-02 14:01:48

--- Modules/Users/routes/web.php
+++ Modules/Users/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Users\Http\Controllers\UsersController;
+use Modules\Users\app\Http\Controllers\UsersController;
 
 /**
  * Web routes (Inertia panel)
