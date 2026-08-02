c4287de6b5b142d29b44d275376746b6b9264fe2
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:58:17 +0330

    Auto Save 2026-08-02 14:58:17

--- Modules/Users/app/Http/Controllers/Requests/UpdateUserRequest.php
+++ Modules/Users/app/Http/Controllers/Requests/UpdateUserRequest.php
@@ -1,18 +1,69 @@
 <?php
 
-namespace Modules\Users\Http\app\Requests;
+namespace Modules\Users\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateUserRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
     public function rules(): array
     {
+
+        $userId = $this->route('user');
+
+
         return [
-            'name' => 'sometimes|string',
-            'mobile' => 'sometimes|string',
-            'password' => 'nullable|min:6',
-            'role' => 'nullable|string',
+
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+
+
+            'mobile' => [
+                'required',
+                'string',
+                'max:20',
+                Rule::unique('users','mobile')
+                    ->ignore($userId)
+            ],
+
+
+            'email' => [
+                'nullable',
+                'email',
+                'max:255',
+                Rule::unique('users','email')
+                    ->ignore($userId)
+            ],
+
+
+            'password' => [
+                'nullable',
+                'string',
+                'min:8'
+            ],
+
+
+            'status' => [
+                'required',
+                'in:active,inactive'
+            ],
+
+
+            'role' => [
+                'required',
+                'exists:roles,name'
+            ],
+
         ];
     }
 }
