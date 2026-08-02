558b111f879e77006d0f656a5c6f2fd6b49538e8
Shamim <shamimtabasi6@gmail.com>
2026-08-02 12:40:06 +0330

    Auto Save 2026-08-02 12:40:06

--- Modules/Sms/app/Providers/SmsServiceProvider.php
+++ Modules/Sms/app/Providers/SmsServiceProvider.php
@@ -4,14 +4,14 @@ namespace Modules\Sms\app\Providers;
 
 use App\Support\BaseModuleServiceProvider;
 use Modules\Sms\app\Services\SmsService;
-use Modules\Sms\app\Repositories\SmsRepository;
+// use Modules\Sms\app\Repositories\SmsRepository;
 
 class SmsServiceProvider extends BaseModuleServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(SmsService::class);
-        $this->app->singleton(SmsRepository::class);
+        // $this->app->singleton(SmsRepository::class);
     }
 
     public function boot(): void
