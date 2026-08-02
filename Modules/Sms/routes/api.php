61a44e99430339052f15d5eb7d26733ae1ce5de3
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:12:45 +0330

    Auto Save 2026-08-02 14:12:45

--- Modules/Sms/routes/api.php
+++ Modules/Sms/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Sms\Http\Controllers\SmsController;
+use Modules\Sms\app\Http\Controllers\SmsController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('sms', SmsController::class)->names('sms');
