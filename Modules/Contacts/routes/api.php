58d4c4f435b7274730dbd2fad0313ff8427b2e11
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:06:59 +0330

    Auto Save 2026-08-02 14:06:59

--- Modules/Contacts/routes/api.php
+++ Modules/Contacts/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Contacts\Http\Controllers\ContactsController;
+use Modules\Contacts\app\Http\Controllers\ContactsController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('contacts', ContactsController::class)->names('contacts');
