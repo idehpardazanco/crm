296b15f92f0169ed3f0688f415f8a1bdfba946de
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:11:09 +0330

    Auto Save 2026-08-02 14:11:09

--- Modules/Core/routes/web.php
+++ Modules/Core/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Core\Http\Controllers\CoreController;
+use Modules\Core\app\Http\Controllers\CoreController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('cores', CoreController::class)->names('core');
