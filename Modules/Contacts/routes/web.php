e595d09a2b1e7df05666cc684ca83ec63ce2febf
Shamim <shamimtabasi6@gmail.com>
2026-08-02 11:58:27 +0330

    Auto Save 2026-08-02 11:58:26

--- Modules/Contacts/routes/web.php
+++ Modules/Contacts/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Contacts\Http\Controllers\ContactsController;
+use Modules\Contacts\app\Http\Controllers\ContactsController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('contacts', ContactsController::class)->names('contacts');
