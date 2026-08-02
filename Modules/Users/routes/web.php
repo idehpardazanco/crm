887045a6dcfeb6e658e51028c81daabc21c031d0
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:40:24 +0330

    Auto Save 2026-08-02 14:40:24

--- Modules/Users/routes/web.php
+++ Modules/Users/routes/web.php
@@ -1,20 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Users\app\Http\Controllers\UsersController;
+use Modules\Users\Http\Controllers\UsersController;
 
-/**
- * Web routes (Inertia panel)
- */
-Route::middleware(['web', 'auth'])
-    ->prefix('admin/users')
-    ->name('users.')
-    ->group(function () {
 
-        Route::get('/', [UsersController::class, 'index'])->name('index');
-        Route::get('/create', [UsersController::class, 'create'])->name('create');
-        Route::post('/', [UsersController::class, 'store'])->name('store');
-        Route::get('/{id}/edit', [UsersController::class, 'edit'])->name('edit');
-        Route::put('/{id}', [UsersController::class, 'update'])->name('update');
-        Route::delete('/{id}', [UsersController::class, 'destroy'])->name('destroy');
-    });
+Route::middleware([
+    'auth'
+])
+->prefix('users')
+->name('users.')
+->group(function(){
+
+
+    Route::get('/',[
+        UsersController::class,
+        'index'
+    ])
+    ->name('index');
+
+
+    Route::post('/',[
+        UsersController::class,
+        'store'
+    ])
+    ->name('store');
+
+
+    Route::put('/{id}',[
+        UsersController::class,
+        'update'
+    ])
+    ->name('update');
+
+
+    Route::delete('/{id}',[
+        UsersController::class,
+        'destroy'
+    ])
+    ->name('destroy');
+
+
+});
