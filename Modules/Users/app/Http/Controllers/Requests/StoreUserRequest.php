7e777fa9b8255699437d386bd953507b03f1e484
Shamim <shamimtabasi6@gmail.com>
2026-08-02 14:36:59 +0330

    Auto Save 2026-08-02 14:36:59

--- Modules/Users/app/Http/Controllers/Requests/StoreUserRequest.php
+++ Modules/Users/app/Http/Controllers/Requests/StoreUserRequest.php
@@ -1,18 +1,57 @@
 <?php
 
-namespace Modules\Users\Http\app\Requests;
+namespace Modules\Users\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreUserRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
-            'mobile' => 'required|string|unique:users,mobile',
-            'password' => 'required|min:6',
-            'role' => 'nullable|string',
+
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+
+            'mobile' => [
+                'required',
+                'string',
+                'max:20',
+                'unique:users,mobile'
+            ],
+
+            'email' => [
+                'nullable',
+                'email',
+                'max:255',
+                'unique:users,email'
+            ],
+
+            'password' => [
+                'required',
+                'string',
+                'min:8'
+            ],
+
+            'status' => [
+                'required',
+                'in:active,inactive'
+            ],
+
+            'role' => [
+                'required',
+                'exists:roles,name'
+            ],
+
         ];
     }
 }
