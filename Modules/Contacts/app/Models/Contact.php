1aa5d4283b966e7ab8a5af397ebb89a099afbe55
Shamim <shamimtabasi6@gmail.com>
2026-08-02 15:12:01 +0330

    Auto Save 2026-08-02 15:12:01

--- Modules/Contacts/app/Models/Contact.php
+++ Modules/Contacts/app/Models/Contact.php
@@ -3,13 +3,51 @@
 namespace Modules\Contacts\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
+
 
 class Contact extends Model
 {
+    use SoftDeletes;
+
+
     protected $fillable = [
+
+        'business_name',
+
         'name',
+
         'mobile',
+
+        'phone',
+
         'email',
-        'address'
+
+        'city',
+
+        'category',
+
+        'source',
+
+        'status',
+
+        'assigned_user_id',
+
+        'address',
+
+        'description',
+
     ];
+
+
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(
+            User::class,
+            'assigned_user_id'
+        );
+    }
+
 }
