b59871429fc34c971ff46658c8bd89c4defb0694
Shamim <shamimtabasi6@gmail.com>
2026-08-03 15:27:07 +0330

    Auto Save 2026-08-03 15:27:07

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use Inertia\Inertia;
 
 
