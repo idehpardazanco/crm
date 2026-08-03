b85c3f992f12ba5948856af6fa435d06f39316a6
Shamim <shamimtabasi6@gmail.com>
2026-08-03 11:58:04 +0330

    Auto Save 2026-08-03 11:58:04

---
+++ Modules/Interactions/app/Services/InteractionService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\Interactions\app\Services;
+
+
+use Modules\Interactions\app\Models\Interaction;
+
+
+
+class InteractionService
+{
+
+    public function list(int $contactId)
+    {
+
+        return Interaction::query()
+
+            ->with('user')
+
+            ->where(
+                'contact_id',
+                $contactId
+            )
+
+            ->latest()
+            ->get();
+
+    }
+
+    public function create(array $data)
+    {
+
+        return Interaction::create($data);
+
+    }
+
+    public function delete(int $id)
+    {
+
+        return Interaction::findOrFail($id)
+            ->delete();
+
+    }
+
+}
