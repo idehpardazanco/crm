fe04c2f6263b9080e1eb523a363439d780192dee
Shamim <shamimtabasi6@gmail.com>
2026-08-03 15:33:50 +0330

    Auto Save 2026-08-03 15:33:50

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 use Inertia\Inertia;
-
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -16,32 +15,47 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/dashboard', [
+        DashboardController::class,
+        'index'
+    ])->name('dashboard');
+
+    Route::get('/profile', [
+        ProfileController::class,
+        'edit'
+    ])->name('profile.edit');
+
+    Route::patch('/profile', [
+        ProfileController::class,
+        'update'
+    ])->name('profile.update');
+
+    Route::delete('/profile', [
+        ProfileController::class,
+        'destroy'
+    ])->name('profile.destroy');
+
+});
+
+Route::middleware([
+    'auth',
+    \App\Http\Middleware\EnsureMonitoringAccess::class
+])->group(function () {
+
+    Route::get('/monitoring', function () {
+        return Inertia::render('Monitoring/Index');
+    });
+
+});
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::middleware(['auth', \App\Http\Middleware\EnsureMonitoringAccess::class])
-    ->get('/monitoring', function () {
-        return Inertia::render('Monitoring/Index');
-});
-
-Route::middleware(['auth'])->group(function () {
     Route::get('/settings/sms', function () {
         return Inertia::render('Settings/Sms');
     });
 
 });
-Route::middleware('auth')
-    ->get('/dashboard', [
-        DashboardController::class,
-        'index'
-    ])
-    ->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
