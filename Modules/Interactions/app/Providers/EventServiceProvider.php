7881b2c8680a2834a2162aa03eae339140d0bd89
Shamim <shamimtabasi6@gmail.com>
2026-08-03 11:46:38 +0330

    Auto Save 2026-08-03 11:46:38

--- Modules/Interactions/app/Providers/EventServiceProvider.php
+++ Modules/Interactions/app/Providers/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Interactions\Providers;
+namespace Modules\Interactions\app\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
