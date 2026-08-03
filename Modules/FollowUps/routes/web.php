7f6a91495354774746eea1bab0e744b6fb56968d
Shamim <shamimtabasi6@gmail.com>
2026-08-03 16:00:00 +0330

    Auto Save 2026-08-03 16:00:00

--- Modules/FollowUps/routes/web.php
+++ Modules/FollowUps/routes/web.php
@@ -1,8 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\FollowUps\Http\Controllers\FollowUpsController;
+use Modules\FollowUps\app\Http\Controllers\FollowUpsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('followups', FollowUpsController::class)->names('followups');
-});
+Route::middleware('auth')
+    ->prefix('followups')
+    ->name('followups.')
+    ->group(function () {
+
+        Route::get('/', [
+            FollowUpsController::class,
+            'index'
+        ])->name('index');
+
+
+        Route::get('/create', [
+            FollowUpsController::class,
+            'create'
+        ])->name('create');
+
+
+        Route::post('/', [
+            FollowUpsController::class,
+            'store'
+        ])->name('store');
+
+
+        Route::delete('/{id}', [
+            FollowUpsController::class,
+            'destroy'
+        ])->name('destroy');
+
+    });
