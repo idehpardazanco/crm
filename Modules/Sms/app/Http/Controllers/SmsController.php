cd1e193a8a80b27821144e18b9ab86a052154027
Shamim <shamimtabasi6@gmail.com>
2026-08-03 13:59:35 +0330

    Auto Save 2026-08-03 13:59:35

--- Modules/Sms/app/Http/Controllers/SmsController.php
+++ Modules/Sms/app/Http/Controllers/SmsController.php
@@ -2,49 +2,23 @@
 
 namespace Modules\Sms\app\Http\Controllers;
 
-
 use Illuminate\Routing\Controller;
-
 use Modules\Sms\app\Http\Requests\SendSmsRequest;
 use Modules\Sms\app\Services\SmsService;
 
-
-
 class SmsController extends Controller
 {
-
-
-    public function __construct(
-
-        protected SmsService $service
-
-    )
+    public function __construct(protected SmsService $service)
     {
 
     }
 
-
-
-
-
-    public function send(
-        SendSmsRequest $request
-    )
+    public function send(SendSmsRequest $request)
     {
-
-
         return $this->service->send(
-
-            $request->to,
-
-            $request->message,
-
-            $request->contact_id
-
+                $request->to,
+                $request->message,
+                $request->contact_id
         );
-
-
     }
-
-
 }
