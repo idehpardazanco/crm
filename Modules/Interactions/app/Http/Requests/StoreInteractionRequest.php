67a66f846bd9be1f92d47030717937ee527f5e68
Shamim <shamimtabasi6@gmail.com>
2026-08-09 14:49:17 +0330

    Auto Save 2026-08-09 14:49:17

--- Modules/Interactions/app/Http/Requests/StoreInteractionRequest.php
+++ Modules/Interactions/app/Http/Requests/StoreInteractionRequest.php
@@ -4,10 +4,8 @@ namespace Modules\Interactions\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-
 class StoreInteractionRequest extends FormRequest
 {
-
     public function authorize(): bool
     {
         return true;
@@ -15,42 +13,41 @@ class StoreInteractionRequest extends FormRequest
 
     public function rules(): array
     {
-
         return [
-
             'contact_id' => [
                 'required',
-                'exists:contacts,id'
+                'integer',
+                'exists:contacts,id',
             ],
 
             'type' => [
                 'required',
-                'in:call,sms,email,note,meeting'
+                'in:call,sms,email,note,meeting',
             ],
 
             'subject' => [
                 'nullable',
                 'string',
-                'max:255'
+                'max:255',
             ],
 
             'description' => [
                 'nullable',
-                'string'
+                'string',
             ],
 
             'result' => [
+                'required_if:type,call',
                 'nullable',
-                'string'
+                'string',
+                'max:255',
             ],
 
             'next_follow_up' => [
                 'nullable',
-                'date'
+                'date',
+                'after_or_equal:now',
             ],
-
         ];
-
     }
-
 }
