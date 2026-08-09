58f0113702521c97b22023d79b4516f8fc971140
Shamim <shamimtabasi6@gmail.com>
2026-08-09 14:50:17 +0330

    Auto Save 2026-08-09 14:50:17

--- Modules/Interactions/app/Http/Controllers/InteractionsController.php
+++ Modules/Interactions/app/Http/Controllers/InteractionsController.php
@@ -2,55 +2,51 @@
 
 namespace Modules\Interactions\app\Http\Controllers;
 
-
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
-
 use Modules\Interactions\app\Http\Requests\StoreInteractionRequest;
 use Modules\Interactions\app\Services\InteractionService;
 
-
-
 class InteractionsController extends Controller
 {
-
-
-    public function __construct(protected InteractionService $service)
-    {
-
+    public function __construct(
+        private readonly InteractionService $service
+    ) {
     }
 
     public function index(int $contactId)
     {
-
         return $this->service->list(
             $contactId
         );
-
     }
 
-    public function store(StoreInteractionRequest $request)
-    {
-        return $this->service->create(
-            array_merge(
+    public function store(
+        StoreInteractionRequest $request
+    ): RedirectResponse {
+        $this->service->create([
+            ...$request->validated(),
 
-                $request->validated(),
+            'user_id' =>
+                $request->user()->id,
+        ]);
 
-                [
-                    'user_id'=>auth()->id()
-                ]
+        return back()->with(
+            'success',
+            'تماس با موفقیت ثبت شد.'
+        );
+    }
 
-            )
+    public function destroy(
+        int $id
+    ): RedirectResponse {
+        $this->service->delete(
+            $id
         );
 
+        return back()->with(
+            'success',
+            'گزارش ارتباط حذف شد.'
+        );
     }
-
-    public function destroy(int $id)
-    {
-
-        return $this->service->delete($id);
-
-    }
-
-
 }
