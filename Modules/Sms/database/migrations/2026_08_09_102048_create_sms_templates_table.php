2e92f0554e6b5c073a5b18d0ab6d157eadf69d92
Shamim <shamimtabasi6@gmail.com>
2026-08-09 13:51:16 +0330

    Auto Save 2026-08-09 13:51:16

---
+++ Modules/Sms/database/migrations/2026_08_09_102048_create_sms_templates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('sms_templates', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+
+            $table->text('body');
+
+            $table->string('type', 100)
+                ->nullable();
+
+            $table->string('status', 20)
+                ->default('active')
+                ->index();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('sms_templates');
+    }
+};
