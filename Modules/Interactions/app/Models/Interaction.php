a7515112308f960e28efa90c3136fc0f755eff95
Shamim <shamimtabasi6@gmail.com>
2026-08-09 15:03:54 +0330

    Auto Save 2026-08-09 15:03:54

--- Modules/Interactions/app/Models/Interaction.php
+++ Modules/Interactions/app/Models/Interaction.php
@@ -2,55 +2,38 @@
 
 namespace Modules\Interactions\app\Models;
 
-
-use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Contacts\app\Models\Contact;
-
-
 
 class Interaction extends Model
 {
     protected $fillable = [
-
         'contact_id',
-
         'user_id',
-
         'type',
-
         'subject',
-
         'description',
-
         'result',
-
+        'status_after_call',
         'next_follow_up',
-
     ];
 
     protected $casts = [
-
-        'next_follow_up'=>'datetime'
-
+        'next_follow_up' => 'datetime',
     ];
 
     public function contact()
     {
-
         return $this->belongsTo(
             Contact::class
         );
-
     }
 
     public function user()
     {
-
         return $this->belongsTo(
             User::class
         );
-
     }
-
 }
