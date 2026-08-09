27820f45951d6eb0cc352f1c1c96a23e88fdb02c
Shamim <shamimtabasi6@gmail.com>
2026-08-09 13:34:55 +0330

    Auto Save 2026-08-09 13:34:55

--- Modules/Sms/app/Providers/PayamMatniSmsProvider.php
+++ Modules/Sms/app/Providers/PayamMatniSmsProvider.php
@@ -3,17 +3,38 @@
 namespace Modules\Sms\app\Providers;
 
 use Illuminate\Support\Facades\Http;
+use Modules\Settings\app\Models\Setting;
 use Modules\Sms\app\Contracts\SmsProviderInterface;
 
 class PayamMatniSmsProvider implements SmsProviderInterface
 {
-    public function send(string $mobile, string $message, ?string $from = null): array
-    {
-        $username = config('sms.providers.payam_matni.username');
-        $password = config('sms.providers.payam_matni.password');
-        $sender = $from ?: config('sms.providers.payam_matni.from');
+    public function send(
+        string $mobile,
+        string $message,
+        ?string $from = null
+    ): array {
+        $username = $this->setting(
+            'sms_username',
+            config(
+                'sms.providers.payam_matni.username'
+            )
+        );
 
-        $payload = [
+        $password = $this->setting(
+            'sms_password',
+            config(
+                'sms.providers.payam_matni.password'
+            )
+        );
+
+        $sender = $from ?: $this->setting(
+            'sms_from',
+            config(
+                'sms.providers.payam_matni.from'
+            )
+        );
+
+        $requestPayload = [
             'method' => 'sendsms',
             'format' => 'json',
             'from' => $sender,
@@ -26,13 +47,38 @@ class PayamMatniSmsProvider implements SmsProviderInterface
 
         $response = Http::timeout(15)
             ->retry(2, 500)
-            ->get('http://payammatni.com/webservice/url/send.php', $payload);
+            ->get(
+                'http://payammatni.com/webservice/url/send.php',
+                $requestPayload
+            );
 
         return [
             'ok' => $response->successful(),
+
             'status' => $response->status(),
-            'payload' => $payload,
-            'body' => $response->json() ?? $response->body(),
+
+            'from' => $sender,
+
+            'payload' => [
+                'method' => 'sendsms',
+                'format' => 'json',
+                'from' => $sender,
+                'to' => $mobile,
+                'text' => $message,
+                'type' => 0,
+            ],
+
+            'body' => $response->json()
+                ?? $response->body(),
         ];
+    }
+
+    private function setting(
+        string $key,
+        mixed $default = null
+    ): mixed {
+        return Setting::query()
+            ->where('key', $key)
+            ->value('value') ?: $default;
     }
 }
