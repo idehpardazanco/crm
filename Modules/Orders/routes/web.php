90926acd8c78741bba4ebbe9f6bafbccd6ffb60a
Shamim <shamimtabasi6@gmail.com>
2026-08-09 16:03:17 +0330

    Auto Save 2026-08-09 16:03:17

--- Modules/Orders/routes/web.php
+++ Modules/Orders/routes/web.php
@@ -3,6 +3,38 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Orders\app\Http\Controllers\OrdersController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('orders', OrdersController::class)->names('orders');
-});
+Route::middleware('auth')
+    ->prefix('orders')
+    ->name('orders.')
+    ->group(function () {
+
+        Route::get('/', [
+            OrdersController::class,
+            'index',
+        ])->name('index');
+
+        Route::get('/create', [
+            OrdersController::class,
+            'create',
+        ])->name('create');
+
+        Route::post('/', [
+            OrdersController::class,
+            'store',
+        ])->name('store');
+
+        Route::get('/{id}/edit', [
+            OrdersController::class,
+            'edit',
+        ])->name('edit');
+
+        Route::put('/{id}', [
+            OrdersController::class,
+            'update',
+        ])->name('update');
+
+        Route::delete('/{id}', [
+            OrdersController::class,
+            'destroy',
+        ])->name('destroy');
+    });
