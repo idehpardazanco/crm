cc544ce16b07fcb1b9e56f3abd406377c96e6b4c
Shamim <shamimtabasi6@gmail.com>
2026-08-09 13:21:44 +0330

    Auto Save 2026-08-09 13:21:44

--- Modules/Users/database/seeders/RolePermissionSeeder.php
+++ Modules/Users/database/seeders/RolePermissionSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace Modules\Users\Database\seeders;
-
+namespace Modules\Users\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
