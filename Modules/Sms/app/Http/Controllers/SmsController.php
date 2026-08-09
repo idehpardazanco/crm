9dc65088be880bb5fdcb7db0b839cf1e94636ac7
Shamim <shamimtabasi6@gmail.com>
2026-08-09 13:36:05 +0330

    Auto Save 2026-08-09 13:36:05

--- Modules/Sms/app/Http/Controllers/SmsController.php
+++ Modules/Sms/app/Http/Controllers/SmsController.php
@@ -2,23 +2,32 @@
 
 namespace Modules\Sms\app\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
 use Modules\Sms\app\Http\Requests\SendSmsRequest;
 use Modules\Sms\app\Services\SmsService;
 
 class SmsController extends Controller
 {
-    public function __construct(protected SmsService $service)
-    {
-
+    public function __construct(
+        private readonly SmsService $service
+    ) {
     }
 
-    public function send(SendSmsRequest $request)
-    {
-        return $this->service->send(
-                $request->to,
-                $request->message,
-                $request->contact_id
+    public function send(
+        SendSmsRequest $request
+    ): RedirectResponse {
+        $data = $request->validated();
+
+        $this->service->send(
+            $data['to'],
+            $data['message'],
+            $data['contact_id'] ?? null
+        );
+
+        return back()->with(
+            'success',
+            'پیامک در صف ارسال قرار گرفت.'
         );
     }
 }
