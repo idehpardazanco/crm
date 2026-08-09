6f35aa88b3ef943698a417836fa97fac62cc07a6
Shamim <shamimtabasi6@gmail.com>
2026-08-09 15:51:29 +0330

    Auto Save 2026-08-09 15:51:29

--- Modules/Sms/app/Http/Controllers/SmsLogsController.php
+++ Modules/Sms/app/Http/Controllers/SmsLogsController.php
@@ -5,24 +5,31 @@ namespace Modules\Sms\app\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Inertia\Inertia;
+use Inertia\Response;
 use Modules\Sms\app\Services\SmsLogService;
 
 class SmsLogsController extends Controller
 {
     public function __construct(
-        protected SmsLogService $service
+        private readonly SmsLogService $service
     ) {
     }
 
+    public function index(
+        Request $request
+    ): Response {
+        return Inertia::render(
+            'Sms/Index',
+            [
+                'logs' =>
+                    $this->service->paginate(
+                        $request
+                            ->string('search')
+                            ->toString(),
 
-    public function index(Request $request)
-    {
-        return Inertia::render('Sms/Index', [
-
-            'logs' => $this->service->paginate(
-                $request->get('search')
-            )
-
-        ]);
+                        $request->user()
+                    ),
+            ]
+        );
     }
 }
