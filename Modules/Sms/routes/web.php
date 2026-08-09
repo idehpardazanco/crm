41309261d9a9d71455f47f371e0c4e0fa6ccaa94
Shamim <shamimtabasi6@gmail.com>
2026-08-09 13:58:07 +0330

    Auto Save 2026-08-09 13:58:07

--- Modules/Sms/routes/web.php
+++ Modules/Sms/routes/web.php
@@ -4,44 +4,63 @@ use Illuminate\Support\Facades\Route;
 use Modules\Sms\app\Http\Controllers\SmsController;
 use Modules\Sms\app\Http\Controllers\SmsLogsController;
 use Modules\Sms\app\Http\Controllers\SmsSettingsController;
+use Modules\Sms\app\Http\Controllers\SmsTemplatesController;
 
-Route::middleware([
-    'web',
-    'auth',
-])
+Route::middleware('auth')
     ->prefix('sms')
     ->name('sms.')
     ->group(function () {
 
-        Route::get(
-            '/settings',
-            [
-                SmsSettingsController::class,
-                'index',
-            ]
-        )->name('settings');
+        Route::get('/templates', [
+            SmsTemplatesController::class,
+            'index',
+        ])->name('templates.index');
 
-        Route::post(
-            '/settings',
-            [
-                SmsSettingsController::class,
-                'update',
-            ]
-        )->name('settings.update');
+        Route::get('/templates/create', [
+            SmsTemplatesController::class,
+            'create',
+        ])->name('templates.create');
 
-        Route::post(
-            '/send',
-            [
-                SmsController::class,
-                'send',
-            ]
-        )->name('send');
+        Route::post('/templates', [
+            SmsTemplatesController::class,
+            'store',
+        ])->name('templates.store');
 
-        Route::get(
-            '/logs',
-            [
-                SmsLogsController::class,
-                'index',
-            ]
-        )->name('logs');
+        Route::get('/templates/{id}/edit', [
+            SmsTemplatesController::class,
+            'edit',
+        ])->name('templates.edit');
+
+        Route::put('/templates/{id}', [
+            SmsTemplatesController::class,
+            'update',
+        ])->name('templates.update');
+
+        Route::delete('/templates/{id}', [
+            SmsTemplatesController::class,
+            'destroy',
+        ])->name('templates.destroy');
+
+
+        Route::get('/settings', [
+            SmsSettingsController::class,
+            'index',
+        ])->name('settings');
+
+        Route::post('/settings', [
+            SmsSettingsController::class,
+            'update',
+        ])->name('settings.update');
+
+
+        Route::post('/send', [
+            SmsController::class,
+            'send',
+        ])->name('send');
+
+
+        Route::get('/logs', [
+            SmsLogsController::class,
+            'index',
+        ])->name('logs');
     });
