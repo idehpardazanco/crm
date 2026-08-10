48cff508d39a813d1e49a840d99a5f28af52e438
Shamim <shamimtabasi6@gmail.com>
2026-08-10 15:05:13 +0330

    Auto Save 2026-08-10 15:05:12

--- Modules/Interactions/routes/api.php
+++ Modules/Interactions/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Modules\Interactions\Http\Controllers\InteractionsController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('interactions', InteractionsController::class)->names('interactions');
-});
+/*
+|--------------------------------------------------------------------------
+| Interactions API
+|--------------------------------------------------------------------------
+|
+| API ارتباطات در نسخه فعلی CRM فعال نیست.
+|
+*/
