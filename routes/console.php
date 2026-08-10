53de0af9f0ff1f7fc9c6f4f018a3bebd54ea1b86
Shamim <shamimtabasi6@gmail.com>
2026-08-10 12:34:00 +0330

    Auto Save 2026-08-10 12:34:00

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,61 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Modules\FollowUps\app\Services\FollowUpReminderService;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Inspire
+|--------------------------------------------------------------------------
+*/
+
+Artisan::command(
+    'inspire',
+    function () {
+        $this->comment(
+            Inspiring::quote()
+        );
+    }
+)->purpose(
+    'Display an inspiring quote'
+);
+
+
+/*
+|--------------------------------------------------------------------------
+| Follow Up Reminder Command
+|--------------------------------------------------------------------------
+*/
+
+Artisan::command(
+    'followups:check-due',
+    function () {
+
+        $processed =
+            app(
+                FollowUpReminderService::class
+            )->processDue();
+
+        $this->info(
+            "{$processed} follow-up(s) processed."
+        );
+
+    }
+)->purpose(
+    'Check and process due CRM follow-ups'
+);
+
+
+/*
+|--------------------------------------------------------------------------
+| Scheduler
+|--------------------------------------------------------------------------
+*/
+
+Schedule::command(
+    'followups:check-due'
+)
+    ->everyMinute()
+    ->withoutOverlapping(5);
