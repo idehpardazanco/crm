6c5c21ce4fdbf14ec4feac81090f95f3235d4d7b
Shamim <shamimtabasi6@gmail.com>
2026-08-10 11:39:40 +0330

    Auto Save 2026-08-10 11:39:40

--- Modules/Contacts/routes/web.php
+++ Modules/Contacts/routes/web.php
@@ -2,44 +2,105 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Contacts\app\Http\Controllers\ContactsController;
+use Modules\Contacts\app\Http\Controllers\ContactsImportController;
 
 Route::middleware('auth')
     ->prefix('contacts')
     ->name('contacts.')
     ->group(function () {
 
-        Route::get('/', [
-            ContactsController::class,
-            'index',
-        ])->name('index');
+        /*
+         * Excel Import
+         *
+         * این Routeها باید قبل از
+         * /{contact}
+         * باشند.
+         */
 
-        Route::get('/create', [
-            ContactsController::class,
-            'create',
-        ])->name('create');
+        Route::get(
+            '/import',
+            [
+                ContactsImportController::class,
+                'index',
+            ]
+        )->name('import');
 
-        Route::post('/', [
-            ContactsController::class,
-            'store',
-        ])->name('store');
+        Route::post(
+            '/import',
+            [
+                ContactsImportController::class,
+                'store',
+            ]
+        )->name('import.store');
 
-        Route::get('/{contact}/edit', [
-            ContactsController::class,
-            'edit',
-        ])->name('edit');
+        Route::get(
+            '/import/template',
+            [
+                ContactsImportController::class,
+                'template',
+            ]
+        )->name('import.template');
 
-        Route::put('/{contact}', [
-            ContactsController::class,
-            'update',
-        ])->name('update');
 
-        Route::delete('/{contact}', [
-            ContactsController::class,
-            'destroy',
-        ])->name('destroy');
+        Route::get(
+            '/',
+            [
+                ContactsController::class,
+                'index',
+            ]
+        )->name('index');
 
-        Route::get('/{contact}', [
-            ContactsController::class,
-            'show',
-        ])->name('show');
+
+        Route::get(
+            '/create',
+            [
+                ContactsController::class,
+                'create',
+            ]
+        )->name('create');
+
+
+        Route::post(
+            '/',
+            [
+                ContactsController::class,
+                'store',
+            ]
+        )->name('store');
+
+
+        Route::get(
+            '/{contact}/edit',
+            [
+                ContactsController::class,
+                'edit',
+            ]
+        )->name('edit');
+
+
+        Route::put(
+            '/{contact}',
+            [
+                ContactsController::class,
+                'update',
+            ]
+        )->name('update');
+
+
+        Route::delete(
+            '/{contact}',
+            [
+                ContactsController::class,
+                'destroy',
+            ]
+        )->name('destroy');
+
+
+        Route::get(
+            '/{contact}',
+            [
+                ContactsController::class,
+                'show',
+            ]
+        )->name('show');
     });
