a383d11782751f951b37606a79c8b80a9f819ab5
Shamim <shamimtabasi6@gmail.com>
2026-08-10 10:54:03 +0330

    Auto Save 2026-08-10 10:54:03

--- Modules/Monitoring/routes/api.php
+++ Modules/Monitoring/routes/api.php
@@ -1,12 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Monitoring\app\Http\Controllers\MonitoringController;
-
-Route::prefix('v1/monitoring')->group(function () {
-
-    Route::get('/activities', [MonitoringController::class, 'activities']);
-    Route::get('/system-logs', [MonitoringController::class, 'systemLogs']);
-    Route::get('/request-logs', [MonitoringController::class, 'requestLogs']);
-
-});
