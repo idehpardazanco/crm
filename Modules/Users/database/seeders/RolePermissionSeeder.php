bddcc0b6875ff617dfd3c03c2106b396226568dc
Shamim <shamimtabasi6@gmail.com>
2026-08-10 12:52:34 +0330

    Auto Save 2026-08-10 12:52:34

--- Modules/Users/database/seeders/RolePermissionSeeder.php
+++ Modules/Users/database/seeders/RolePermissionSeeder.php
@@ -1,88 +1,64 @@
 <?php
 
 namespace Modules\Users\Database\Seeders;
+
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class RolePermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        /*
+        |--------------------------------------------------------------------------
+        | Clear Permission Cache
+        |--------------------------------------------------------------------------
+        */
 
-        $permissions = [
-            'dashboard.view',
+        app(
+            PermissionRegistrar::class
+        )->forgetCachedPermissions();
 
-            'users.view',
-            'users.create',
-            'users.update',
-            'users.delete',
 
-            'businesses.view',
-            'businesses.create',
-            'businesses.update',
-            'businesses.delete',
+        /*
+        |--------------------------------------------------------------------------
+        | Super Admin
+        |--------------------------------------------------------------------------
+        */
 
-            'calls.view',
-            'calls.create',
+        Role::query()->firstOrCreate([
+            'name' =>
+                'super_admin',
 
-            'reminders.view',
-            'reminders.create',
-            'reminders.update',
-
-            'sms.templates.view',
-            'sms.templates.create',
-            'sms.templates.update',
-            'sms.templates.delete',
-
-            'sms.logs.view',
-            'sms.send',
-
-            'orders.view',
-            'orders.create',
-            'orders.update',
-
-            'settings.view',
-            'settings.update',
-
-            'monitoring.view',
-        ];
-
-        foreach ($permissions as $permission) {
-            Permission::firstOrCreate([
-                'name' => $permission,
-                'guard_name' => 'web',
-            ]);
-        }
-
-        $superAdmin = Role::firstOrCreate([
-            'name' => 'super_admin',
-            'guard_name' => 'web',
+            'guard_name' =>
+                'web',
         ]);
 
-        $employee = Role::firstOrCreate([
-            'name' => 'employee',
-            'guard_name' => 'web',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Employee
+        |--------------------------------------------------------------------------
+        */
+
+        Role::query()->firstOrCreate([
+            'name' =>
+                'employee',
+
+            'guard_name' =>
+                'web',
         ]);
 
-        $superAdmin->syncPermissions($permissions);
 
-        $employee->syncPermissions([
-            'dashboard.view',
-            'businesses.view',
-            'businesses.create',
-            'businesses.update',
-            'calls.view',
-            'calls.create',
-            'reminders.view',
-            'reminders.create',
-            'reminders.update',
-            'sms.templates.view',
-            'sms.send',
-            'sms.logs.view',
-            'orders.view',
-            'orders.create',
-        ]);
+        /*
+        |--------------------------------------------------------------------------
+        | Clear Cache Again
+        |--------------------------------------------------------------------------
+        */
+
+        app(
+            PermissionRegistrar::class
+        )->forgetCachedPermissions();
     }
 }
