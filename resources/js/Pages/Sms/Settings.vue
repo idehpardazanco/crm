852cab2dcba44bafe23bbe8969715a226072fcce
Shamim <shamimtabasi6@gmail.com>
2026-08-17 11:04:10 +0330

    Auto Save 2026-08-17 11:04:09

--- resources/js/Pages/Sms/Settings.vue
+++ resources/js/Pages/Sms/Settings.vue
@@ -2,26 +2,19 @@
 import { useForm } from '@inertiajs/vue3'
 
 const props = defineProps({
-    settings: Object,
+    settings: {
+        type: Object,
+        default: () => ({}),
+    },
 })
 
 const form = useForm({
-    sms_from:
-        props.settings.sms_from ?? '',
-
-    sms_username:
-        props.settings.sms_username ?? '',
-
+    sms_from: props.settings?.sms_from ?? '',
+    sms_username: props.settings?.sms_username ?? '',
     sms_password: '',
-
-    demo_link:
-        props.settings.demo_link ?? '',
-
-    product_name:
-        props.settings.product_name ?? '',
-
-    order_link:
-        props.settings.order_link ?? '',
+    demo_link: props.settings?.demo_link ?? '',
+    product_name: props.settings?.product_name ?? '',
+    order_link: props.settings?.order_link ?? '',
 })
 
 const submit = () => {
@@ -37,131 +30,159 @@ const submit = () => {
 
 <template>
     <div class="p-6">
-
-        <h1 class="text-xl font-bold mb-6">
+        <h1 class="mb-6 text-xl font-bold">
             تنظیمات پیامک
         </h1>
 
         <form
-            @submit.prevent="submit"
             class="max-w-2xl"
+            @submit.prevent="submit"
         >
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     شماره فرستنده
                 </label>
 
                 <input
                     v-model="form.sms_from"
                     type="text"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                 >
 
+                <div
+                    v-if="form.errors.sms_from"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.sms_from }}
+                </div>
             </div>
 
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     نام کاربری پنل پیامک
                 </label>
 
                 <input
                     v-model="form.sms_username"
                     type="text"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                 >
 
+                <div
+                    v-if="form.errors.sms_username"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.sms_username }}
+                </div>
             </div>
 
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     رمز عبور پنل پیامک
                 </label>
 
                 <input
                     v-model="form.sms_password"
                     type="password"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                     placeholder="برای عدم تغییر خالی بگذارید"
                 >
 
+                <div
+                    v-if="form.errors.sms_password"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.sms_password }}
+                </div>
             </div>
-
 
             <hr class="my-6">
 
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     لینک دمو
                 </label>
 
                 <input
                     v-model="form.demo_link"
                     type="text"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                 >
 
-                <div class="text-sm text-gray-500 mt-1">
-                    {{ '{{demo_link}}' }}
+                <div
+                    v-pre
+                    class="mt-1 text-sm text-gray-500"
+                >
+                    {{demo_link}}
                 </div>
 
+                <div
+                    v-if="form.errors.demo_link"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.demo_link }}
+                </div>
             </div>
 
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     نام محصول
                 </label>
 
                 <input
                     v-model="form.product_name"
                     type="text"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                 >
 
-                <div class="text-sm text-gray-500 mt-1">
-                    {{ '{{product_name}}' }}
+                <div
+                    v-pre
+                    class="mt-1 text-sm text-gray-500"
+                >
+                    {{product_name}}
                 </div>
 
+                <div
+                    v-if="form.errors.product_name"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.product_name }}
+                </div>
             </div>
 
-
             <div class="mb-4">
-
-                <label class="block mb-2">
+                <label class="mb-2 block">
                     لینک سفارش
                 </label>
 
                 <input
                     v-model="form.order_link"
                     type="text"
-                    class="border p-2 w-full"
+                    class="w-full border p-2"
                 >
 
-                <div class="text-sm text-gray-500 mt-1">
-                    {{ '{{order_link}}' }}
+                <div
+                    v-pre
+                    class="mt-1 text-sm text-gray-500"
+                >
+                    {{order_link}}
                 </div>
 
+                <div
+                    v-if="form.errors.order_link"
+                    class="mt-1 text-sm text-red-600"
+                >
+                    {{ form.errors.order_link }}
+                </div>
             </div>
-
 
             <button
                 type="submit"
                 :disabled="form.processing"
-                class="bg-blue-600 text-white px-5 py-2 rounded"
+                class="rounded bg-blue-600 px-5 py-2 text-white disabled:opacity-50"
             >
-                ذخیره تنظیمات
+                {{ form.processing ? 'در حال ذخیره...' : 'ذخیره تنظیمات' }}
             </button>
-
         </form>
-
     </div>
 </template>
